75a0a2078a9a4d52f980503148e60036ab718d04
rturner1989 <robturner1000@hotmail.co.uk>
2021-08-16 18:50:33 +0100

    CSS Update

--- src/Components/VolumeSlider.js
+++ src/Components/VolumeSlider.js
@@ -7,29 +7,31 @@ const VolumeSlider = () => {
     return (
         <div className="volume-container">
             <p>Volume:</p>
-            <BsVolumeDown
-                className="volume-icon"
-                aria-hidden={true}
-                focusable={false}
-            />
-            <label className="label-hidden" htmlFor="volume-slider">
-                Volume Slider
-            </label>
-            <input
-                type="range"
-                min="1"
-                max="100"
-                value={volume}
-                onChange={(e) => volumeControl(e.target.value)}
-                className="volume-slider"
-                id="volume-slider"
-                disabled={!powerToggle}
-            />
-            <BsVolumeUp
-                className="volume-icon"
-                aria-hidden={true}
-                focusable={false}
-            />
+            <div className="volume-slider-container">
+                <BsVolumeDown
+                    className="volume-icon"
+                    aria-hidden={true}
+                    focusable={false}
+                />
+                <label className="label-hidden" htmlFor="volume-slider">
+                    Volume Slider
+                </label>
+                <input
+                    type="range"
+                    min="1"
+                    max="100"
+                    value={volume}
+                    onChange={(e) => volumeControl(e.target.value)}
+                    className="volume-slider"
+                    id="volume-slider"
+                    disabled={!powerToggle}
+                />
+                <BsVolumeUp
+                    className="volume-icon"
+                    aria-hidden={true}
+                    focusable={false}
+                />
+            </div>
         </div>
     );
 };
